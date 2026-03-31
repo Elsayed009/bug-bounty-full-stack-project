8de1567ce25ec3a61cd9dd2302eb9b30af555c85
Elsayed009 <elsayedelshaerawy@gmail.com>
2026-03-31 14:11:05 +0200

    update v1

--- server/routes/programRoutes.js
+++ server/routes/programRoutes.js
@@ -11,12 +11,21 @@ const {  deleteProgram,
 }= require("../controllers/programController")
     // router.post("/program",verifyToken, createProgram);
     router.post("/program", verifyToken, validate(programSchema), createProgram);
+    router.get("/active", getAllActivePrograms);
     router.get("/programs",verifyToken, getPrograms);
     router.get("/programs/:id",verifyToken, getProgram);
     // router.put("/program/:id",verifyToken, updateProgram);
     router.put("/program/:id", verifyToken, validate(programSchema), updateProgram);
     router.delete("/program/:id",verifyToken, deleteProgram);
-    router.delete("/active", getAllActivePrograms);
 
     module.exports = router;
+
+
+
+
+//     الـ routes الـ specific   → فوق
+// الـ routes الـ dynamic    → تحت
+
+// ✅ /active   ← specific
+// ✅ /:id      ← dynamic (لازم يكون بعده)
 
